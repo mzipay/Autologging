aa3cb04b22d6b852845a9f4b2e6bf32d9b2092f6
Matthew Zipay <mattz@ninthtest.info>
2018-08-23 10:25:23 -0400

    now using markdown long description for PyPI (https://dustingram.com/articles/2018/03/16/markdown-descriptions-on-pypi)

--- setup.py
+++ setup.py
@@ -2,60 +2,15 @@
 
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="Autologging",
     version="1.2.0b0",
     description="Autologging makes logging and tracing Python classes easy.",
-    long_description="""\
-Autologging eliminates boilerplate logging setup code and tracing code,
-and provides a means to separate application logging from program flow
-and data tracing.
-
-Python modules that make use of Autologging are cleaner, leaner, and
-more resilient to changes that would otherwise require updating tracing
-statements.
-
-Autologging allows for tracing to be configured (and controlled)
-independently from application logging. Toggle tracing on/off, write
-trace log records to a separate log, and use different formatting for
-trace log entries - all via standard Python logging facilities, and
-without affecting your application logging.
-
-Autologging exposes two decorators and a custom log level:
-
-**@logged**
-Decorate a class to create a ``__log`` member. The logger is named by
-default to match the dotted-name of the containing class. A function
-may also be decorated, creating a ``_log`` attribute on the function
-object whose default name matches the containing module.
-A specifically-named logger may also be passed to the decorator (i.e.
-``@logged(my_logger)``).
-
-**@traced**
-Decorate a class to provide **automatic** method call/return tracing. By
-default, all class, static, and instance methods are traced (excluding
-"__special__" methods, with the exception of ``__init__`` and ``__call__``).
-As with the ``logged`` decorator, the default name of the tracing logger
-matches the dotted-name of the containing class and may be overridden by
-passing a specifically-named logger to the decorator.
-Additionally, this decorator accepts multiple string arguments that
-explicitly name the methods to be traced (and may even name
-"__special__" methods).
-
-Module-level functions may also be traced using this decorator.
-
-*New in version 1.2.0:* automatic yield/stop tracing of Python
-generator iterators (if the generator function is traced).
-
-**TRACE**
-The ``autologging.TRACE`` (level 1) log level is registered with the
-Python ``logging`` module when Autologging is imported so that tracing
-can be configured and controlled independently of application logging.
-
-Tracing may be disabled entirely by setting the
-``AUTOLOGGING_TRACED_NOOP`` environment variable or by calling the
-``autologging.install_traced_noop()`` function.
-""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Matthew Zipay",
     author_email="mattz@ninthtest.info",
     url="http://ninthtest.info/python-autologging/",
